8eb43f3ac389fd659b766e53dd5e64789ea7fb79
root <root@Furkan>
2024-02-12 18:08:54 +0300

    Implemented FizzBuzz with range and Factorial

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-fn gcd(a: u32, b: u32) -> u32 {
-    if b > 0 {
-        gcd(b, a % b)
-    } else {
-        a
+fn factorial(n: u32) -> u32 {
+    let mut product = 1;
+    for i in 1..=n {
+        product *= dbg!(i);
+    }
+    product
+}
+
+fn fizzbuzz(n: u32) {
+    for i in 0..=n {
+        if i % 3 == 0 {
+            println!("{i} is divisible by 3");
+        } else if i % 5 == 0 {
+            println!("{i} is divisible by 5");
+        } else if i % 3 == 0 && i % 5 == 0 {
+            println!("{i} is divisible by both 3 and 5");
+        } else {
+            println!("{i} is neither divisible by 3 or 5");
+        }
     }
 }
 
 fn main() {
-    println!("gcd: {}", gcd(143, 52));
+    fizzbuzz(44);
+    let n = 4;
+    println!("{n}! = {}", factorial(n));
 }
